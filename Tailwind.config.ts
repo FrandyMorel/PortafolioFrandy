2e656c03d1d5236603975ddd71d6424d6a599d8a
Frandy Morel <frandymorel3@gmail.com>
2026-03-01 21:54:38 -0400

    Configuracion de estilos

--- Tailwind.config.ts
+++ Tailwind.config.ts
@@ -9,7 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        secondary: "#b5741c",
+        secondary: "#f5741c",
+        darkbg: "#131424",
+      },
+      backgroundImage: {
+        "gradient-cover":
+          "linear-gradient(90.21deg, rgba(70, 54, 124, 0.5) -5.9%, rgba(74, 47, 189, 0.5) 111.58%)",
       },
     },
   },
